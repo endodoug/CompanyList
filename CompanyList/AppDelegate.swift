2ceb0cdddb498be9595471d299c67091c7afcbb8
Doug Harper <doug@zewkey.com>
2017-10-21 21:32:35 -0500

    created a NavController with a blue (dummy) root view

--- CompanyList/AppDelegate.swift
+++ CompanyList/AppDelegate.swift
@@ -15,7 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    
+    window = UIWindow()
+    window?.makeKeyAndVisible()
+    
+    let dummyViewController = UIViewController()
+    dummyViewController.view.backgroundColor = .blue
+    let navController = UINavigationController(rootViewController: dummyViewController)
+    window?.rootViewController = navController
+    
     return true
   }
 
