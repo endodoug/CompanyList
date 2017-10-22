8182e56f69838146afd156a883ce5e00d63b48ef
Doug Harper <doug@zewkey.com>
2017-10-21 21:22:06 -0500

    Initial Commit

---
+++ CompanyList/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CompanyList
+//
+//  Created by doug harper on 10/21/17.
+//  Copyright © 2017 Doug Harper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+   
+    
+    
+  }
+
+
+}
+
