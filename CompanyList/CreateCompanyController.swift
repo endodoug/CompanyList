948ade885946c44648efd35fe2a67ca6102c920b
Doug Harper <doug@zewkey.com>
2017-10-24 13:14:05 -0500

    Created a name label and Text Field for the CreateCompanyController

--- CompanyList/CreateCompanyController.swift
+++ CompanyList/CreateCompanyController.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class CreateCompanyController: UIViewController {
+  
+  let nameLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Name"
+    label.font = UIFont.boldSystemFont(ofSize: 16)
+    label.textColor = ThemeColor.asphalt
+    
+    return label
+  }()
+  
+  let nameTextField: UITextField = {
+    let textField = UITextField()
+    textField.placeholder = "Enter Company Name"
+    return textField
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -16,7 +32,25 @@ class CreateCompanyController: UIViewController {
     navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancelTapped))
     
     view.backgroundColor = ThemeColor.asphalt
+    setUpUI()
     
+  }
+  
+  func setUpUI() {
+    let khakiBackgroundView: UIView = {
+      let view = UIView()
+      view.backgroundColor = ThemeColor.khaki
+      return view
+    }()
+    
+    view.addSubview(khakiBackgroundView)
+    khakiBackgroundView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+    
+    view.addSubview(nameLabel)
+    nameLabel.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 16, paddingBottom: 0, paddingRight: 0, width: 100, height: 50)
+    
+    view.addSubview(nameTextField)
+    nameTextField.anchor(top: nameLabel.topAnchor, left: nameLabel.rightAnchor, bottom: nameLabel.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
   }
   
   @objc func handleCancelTapped() {
