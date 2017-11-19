95313f61c79750a79b09ff80ccc45da45c11bdb7
Doug Harper <doug@zewkey.com>
2017-11-18 22:19:22 -0600

    Inline filter for Staff even easier, but i think i prefer the seniorManagement way of doing it

--- CompanyList/Companies/Employees/EmployeeController.swift
+++ CompanyList/Companies/Employees/EmployeeController.swift
@@ -43,7 +43,8 @@ class EmployeeController: UITableViewController {
     
     allEmployeeNames = [
       executive,
-      seniorManagement
+      seniorManagement,
+      companyEmployees.filter { $0.type == "Staff" }
     ]
     
   }
