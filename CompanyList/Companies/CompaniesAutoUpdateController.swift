463e2d3eb0e5ce63cc23ddd28c57da9cc925030e
Doug Harper <doug@zewkey.com>
2018-02-26 12:43:33 -0600

    TableView Cells

--- CompanyList/Companies/CompaniesAutoUpdateController.swift
+++ CompanyList/Companies/CompaniesAutoUpdateController.swift
@@ -37,9 +37,27 @@ class CompaniesAutoUpdateController: UITableViewController {
     navigationItem.title = "company auto updates"
     
     tableView.backgroundColor = UIColor.darkGray
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     
     fetchedResultsController.fetchedObjects?.forEach({ (company) in
       print(company.name ?? "")
     })
   }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return fetchedResultsController.sections![section].numberOfObjects
+}
+
+  let cellId = "cellId"
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+    
+    
+    return cell
+  }
+
+
+
+
 }
