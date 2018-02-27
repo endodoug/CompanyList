d80ffe66ebdc32ac96f911d65a298743f57d9153
Doug Harper <doug@zewkey.com>
2018-02-26 21:45:35 -0600

    Data Changes to Data Table Added the methods for updating the tableview data

--- CompanyList/Companies/CompaniesAutoUpdateController.swift
+++ CompanyList/Companies/CompaniesAutoUpdateController.swift
@@ -33,12 +33,46 @@ class CompaniesAutoUpdateController: UITableViewController, NSFetchedResultsCont
     return frc
   }()
   
+  func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    tableView.beginUpdates()
+  }
+  
+  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+    switch type {
+    case .insert:
+      tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+    case .delete:
+      tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+    case .move:
+      break
+    case .update:
+      break
+    }
+  }
+  
+  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+    switch type {
+    case .insert:
+      tableView.insertRows(at: [newIndexPath!], with: .fade)
+    case .delete:
+      tableView.deleteRows(at: [indexPath!], with: .fade)
+    case .update:
+      tableView.reloadRows(at: [indexPath!], with: .fade)
+    case .move:
+      tableView.moveRow(at: indexPath!, to: newIndexPath!)
+    }
+  }
+  
+  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    tableView.endUpdates()
+  }
+  
   @objc private func handleAdd() {
     
     print("Add a company called BMW")
     let context = CoreDataManager.shared.persistentContainer.viewContext
     let company = Company(context: context)
-    company.name = "BMW"
+    company.name = "zzz"
     
     do {
       try context.save()
