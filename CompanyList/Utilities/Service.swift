ef90a3245c25e0b88230dc2d3585e0cc7595ba9b
Doug Harper <doug@zewkey.com>
2018-02-27 21:52:50 -0600

    Load JSON From URL

--- CompanyList/Utilities/Service.swift
+++ CompanyList/Utilities/Service.swift
@@ -12,8 +12,24 @@ struct Service {
   
   static let shared = Service()
   
+  let urlString = "https://api.letsbuildthatapp.com/intermediate_training/companies"
+  
+  
+  
   func downloadCompaniesFromServer() {
-    print("Downloading JSON Data from Server")
+    guard let url = URL(string: urlString) else { return }
+    
+    URLSession.shared.dataTask(with: url) { (data, resp, err) in
+      print("Finish Downloading")
+      
+      if let err = err {
+        print("Failed to download JSON:", err)
+      }
+      // could check response here too
+      let string = String(data: data!, encoding: .utf8)
+      print(string)
+      
+      }.resume()
   }
   
   
